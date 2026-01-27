863a8cc2a7ad0fa8a69677307a8372ffba4ae643
Raghav Subramani <raghavsubramani333@gmail.com>
2026-01-27 23:32:11 +0530

    Added Failure, CI Artifacts

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,9 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
   },
 
   expect: {
